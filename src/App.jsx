e7ff369427b8a92fe141490c3f68ae094eba22b3
Leandro Junior <ltorres.external@axa-assistance.us>
2024-07-03 22:38:32 -0300

    remove import UserList

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import AddUserList from './component/AddUserList/AddUserList'
-import UserList from './component/UserList/UserList'
 import { ModalProvider } from './context/modalContext'
 
 function App() {
 
   return (
     <>
-      {/* <UserList /> */}
       <ModalProvider>
         <AddUserList />
       </ModalProvider>
